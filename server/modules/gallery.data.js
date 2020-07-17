24dfb5cca1efd7a6cc3ccb49514387e0ecd88a9b
Skyler Burgard <skylerrburgard@hotmail.com>
2020-07-17 16:36:43 -0500

    Initial commit/ images loaded in.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,54 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+  {
+    id: 0,
+    path: 'images/goat_small.jpg',
+    description: 'A goat -- is it screaming?',
+    likes: 0,
+  },
+  {
+    id: 1,
+    path: 'images/buschstadium.jpg',
+    description: 'The beautiful Busch Stadium located in St.louis, Missouri.',
+    likes: 0,
+  },
+  {
+    id: 2,
+    path: 'devolvedparliament.jpg',
+    description: 'A painting by the great and mysterious, Banksy.',
+    likes: 0,
+  },
+  {
+    id: 3,
+    path: 'gym.jpg',
+    description: 'One of the main constants in my life, the gym.',
+    likes: 0,
+  },
+  {
+    id: 4,
+    path: 'michelbasquait.jpg',
+    description: 'A photo of the late Jean-Michel Basquiat.',
+    likes: 0,
+  },
+  {
+    id: 5,
+    path: 'normanjaffe.jpg',
+    description: 'The Schlachter designed by the amazing Norman Jaffe.',
+    likes: 0,
+  },
+  {
+    id: 6,
+    path: 'danielarshamporche.jpg',
+    description:
+      'A crystal eroded, fully functional Porche 911 designed by artist, Daniel Arsham.',
+    likes: 0,
+  },
+  {
+    id: 7,
+    path: 'stlblues.jpg',
+    description:
+      'the 2019 St.Louis Blues NHL team, posed with their first Stanley Cup in 50 years!',
+    likes: 0,
+  },
 ];
 
 module.exports = galleryItems;
